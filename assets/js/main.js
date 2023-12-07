7dc41b69fc4ccd9ca94a0fdad01b32ee0d858706
thrsequence <qamakarov@gmail.com>
2023-12-07 15:18:46 +0100

    final profect

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,55 +1,90 @@
 'use strict';
 
-/* PRELOAD loading will be end after document is loaded */
-
-const preloader = document.querySelector('[data-preaload]');
-
-window.addEventListener('load', function () {
-    preloader.classList.add('loaded');
-    document.body.classList.add('loaded');
-});
-
-/* add event listener on multiple elements */
-
-const addEventOnElements = function (elements, eventType, callback) {
-    for (let i = 0, len = elements.length; i < len; i++) {
-        elements[i].addEventListener(eventType, callback);
-    }
+// Helper function to add event listeners on multiple elements
+const addEventOnElements = (elements, eventType, callback) => {
+    elements.forEach(element => element.addEventListener(eventType, callback));
 };
 
-/* NAVBAR */
-
-const navbar = document.querySelector('[data-navbar]');
-const navTogglers = document.querySelectorAll('[data-nav-toggler]');
-const overlay = document.querySelector('[data-overlay]');
-
-const toggleNavbar = function () {
+// Helper function to toggle the active state of the navbar
+const toggleNavbar = () => {
     navbar.classList.toggle('active');
     overlay.classList.toggle('active');
     document.body.classList.toggle('nav-active');
 };
 
-addEventOnElements(navTogglers, 'click', toggleNavbar);
-
-/* HEADER & BACK TOP BTN */
-
-const header = document.querySelector('[data-header]');
-const backTopBtn = document.querySelector('[data-back-top-btn]');
-
-let lastScrollPos = 0;
-
-const hideHeader = function () {
+// Helper function to hide the header based on scroll position
+const hideHeader = () => {
     const isScrollBottom = lastScrollPos < window.scrollY;
-    if (isScrollBottom) {
-        header.classList.add('hide');
-    } else {
-        header.classList.remove('hide');
-    }
-
+    header.classList.toggle('hide', isScrollBottom);
     lastScrollPos = window.scrollY;
 };
 
-window.addEventListener('scroll', function () {
+// Helper function to update the position of the hero slider
+const updateSliderPos = () => {
+    lastActiveSliderItem.classList.remove('active');
+    heroSliderItems[currentSlidePos].classList.add('active');
+    lastActiveSliderItem = heroSliderItems[currentSlidePos];
+};
+
+// Helper function to handle the next slide in the hero slider
+const slideNext = () => {
+    currentSlidePos = (currentSlidePos >= heroSliderItems.length - 1) ? 0 : currentSlidePos + 1;
+    updateSliderPos();
+};
+
+// Helper function to handle the previous slide in the hero slider
+const slidePrev = () => {
+    currentSlidePos = (currentSlidePos <= 0) ? heroSliderItems.length - 1 : currentSlidePos - 1;
+    updateSliderPos();
+};
+
+// Helper function to start auto-sliding and clear interval on mouseover
+const handleAutoSlide = () => {
+    autoSlideInterval = setInterval(slideNext, 7000);
+};
+
+// Helper function to stop auto-slide on mouseover and resume on mouseout
+const stopAutoSlide = () => {
+    clearInterval(autoSlideInterval);
+};
+
+// Helper function to apply parallax effect on mousemove
+const applyParallaxEffect = event => {
+    x = (event.clientX / window.innerWidth) * 10 - 5;
+    y = (event.clientY / window.innerHeight) * 10 - 5;
+    x = x - x * 2;
+    y = y - y * 2;
+
+    parallaxItems.forEach(item => {
+        x = x * Number(item.dataset.parallaxSpeed);
+        y = y * Number(item.dataset.parallaxSpeed);
+        item.style.transform = `translate3d(${x}px, ${y}px, 0px)`;
+    });
+};
+
+// DOM elements
+const preloader = document.querySelector('[data-preaload]');
+const navbar = document.querySelector('[data-navbar]');
+const navTogglers = document.querySelectorAll('[data-nav-toggler]');
+const overlay = document.querySelector('[data-overlay]');
+const header = document.querySelector('[data-header]');
+const backTopBtn = document.querySelector('[data-back-top-btn]');
+const heroSlider = document.querySelector('[data-hero-slider]');
+const heroSliderItems = document.querySelectorAll('[data-hero-slider-item]');
+const heroSliderPrevBtn = document.querySelector('[data-prev-btn]');
+const heroSliderNextBtn = document.querySelector('[data-next-btn]');
+const parallaxItems = document.querySelectorAll('[data-parallax-item]');
+
+// Event listeners
+window.addEventListener('load', () => {
+    preloader.classList.add('loaded');
+    document.body.classList.add('loaded');
+    handleAutoSlide();
+});
+
+addEventOnElements(navTogglers, 'click', toggleNavbar);
+
+window.addEventListener('scroll', () => {
     if (window.scrollY >= 50) {
         header.classList.add('active');
         backTopBtn.classList.add('active');
@@ -60,81 +95,12 @@ window.addEventListener('scroll', function () {
     }
 });
 
-/* HERO SLIDER */
-
-const heroSlider = document.querySelector('[data-hero-slider]');
-const heroSliderItems = document.querySelectorAll('[data-hero-slider-item]');
-const heroSliderPrevBtn = document.querySelector('[data-prev-btn]');
-const heroSliderNextBtn = document.querySelector('[data-next-btn]');
-
-let currentSlidePos = 0;
-let lastActiveSliderItem = heroSliderItems[0];
-
-const updateSliderPos = function () {
-    lastActiveSliderItem.classList.remove('active');
-    heroSliderItems[currentSlidePos].classList.add('active');
-    lastActiveSliderItem = heroSliderItems[currentSlidePos];
-};
-
-const slideNext = function () {
-    if (currentSlidePos >= heroSliderItems.length - 1) {
-        currentSlidePos = 0;
-    } else {
-        currentSlidePos++;
-    }
-
-    updateSliderPos();
-};
-
 heroSliderNextBtn.addEventListener('click', slideNext);
-
-const slidePrev = function () {
-    if (currentSlidePos <= 0) {
-        currentSlidePos = heroSliderItems.length - 1;
-    } else {
-        currentSlidePos--;
-    }
-
-    updateSliderPos();
-};
-
 heroSliderPrevBtn.addEventListener('click', slidePrev);
 
-/* auto slide */
+addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], 'mouseover', stopAutoSlide);
+addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], 'mouseout', handleAutoSlide);
 
-let autoSlideInterval;
+window.addEventListener('load', handleAutoSlide);
 
-const autoSlide = function () {
-    autoSlideInterval = setInterval(function () {
-        slideNext();
-    }, 7000);
-};
-
-addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], 'mouseover', function () {
-    clearInterval(autoSlideInterval);
-});
-
-addEventOnElements([heroSliderNextBtn, heroSliderPrevBtn], 'mouseout', autoSlide);
-
-window.addEventListener('load', autoSlide);
-
-/* PARALLAX EFFECT */
-
-const parallaxItems = document.querySelectorAll('[data-parallax-item]');
-
-let x, y;
-
-window.addEventListener('mousemove', function (event) {
-    x = (event.clientX / window.innerWidth) * 10 - 5;
-    y = (event.clientY / window.innerHeight) * 10 - 5;
-
-    // reverse the number eg. 20 -> -20, -5 -> 5
-    x = x - x * 2;
-    y = y - y * 2;
-
-    for (let i = 0, len = parallaxItems.length; i < len; i++) {
-        x = x * Number(parallaxItems[i].dataset.parallaxSpeed);
-        y = y * Number(parallaxItems[i].dataset.parallaxSpeed);
-        parallaxItems[i].style.transform = `translate3d(${x}px, ${y}px, 0px)`;
-    }
-});
+window.addEventListener('mousemove', applyParallaxEffect);
